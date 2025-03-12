b9f698d2edfcf731047a5beeb37b6160c26da72d
Gael Querharo <gael.querharo@gmail.com>
2025-03-12 05:54:21 +0100

    recup windows

--- src/pages/accueil.tsx
+++ src/pages/accueil.tsx
@@ -1,7 +1,25 @@
+import imprimante from "../img/imprimante.jpg";
+import logomagiccard from "../img/LogoMagiccard.png";
+import cadenas from "../img/cadenastemplate.png";
+
 function accueil() {
   return (
-    <div>
+    <div className="accueil">
       <h1>Découvrez nos produits et nos services</h1>
+
+      <article className="accueil__selection">
+        <div className="accueil__selection__new">NOUVEAUTÉ</div>
+        <img
+          className="accueil__selection__logo"
+          src={logomagiccard}
+          alt="Logo magicard"
+        />
+        <img
+          className="accueil__selection__illustration"
+          src={imprimante}
+          alt="Une imprimante Magicard"
+        />
+      </article>
     </div>
   );
 }
